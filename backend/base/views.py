0a224d9c5b12c218c60c0aaaf6b598b98cc27328
Raymond Oluoch <rasholuoch@gmail.com>
2023-02-23 16:47:16 +0300

    backend setup

---
+++ backend/base/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from .models import Product
+from .serializers import ProductSerializer
+
+# Create your views here.
+
+@api_view(['GET'])
+def getProducts(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getProduct(request, pk):
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
