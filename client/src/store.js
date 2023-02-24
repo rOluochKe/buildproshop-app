adf1bb1b78c43b6278c741664caf4dd2e222854d
Raymond Oluoch <rasholuoch@gmail.com>
2023-02-24 22:29:05 +0300

    checkout process part 1

--- client/src/store.js
+++ client/src/store.js
@@ -16,6 +16,10 @@ import {
   userUpdateProfileReducer,
 } from './reducers/userReducers'
 
+import {
+  orderCreateReducer,
+} from './reducers/orderReducers'
+
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
@@ -26,6 +30,8 @@ const reducer = combineReducers({
   userRegister: userRegisterReducer,
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
+
+  orderCreate: orderCreateReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ?
@@ -34,8 +40,14 @@ const cartItemsFromStorage = localStorage.getItem('cartItems') ?
 const userInfoFromStorage = localStorage.getItem('userInfo') ?
   JSON.parse(localStorage.getItem('userInfo')) : null
 
+const shippingAddressFromStorage = localStorage.getItem('shippingAddress') ?
+  JSON.parse(localStorage.getItem('shippingAddress')) : {}
+
 const initialState = {
-  cart: { cartItems: cartItemsFromStorage },
+  cart: { 
+    cartItems: cartItemsFromStorage,
+    shippingAddress: shippingAddressFromStorage,
+  },
   userLogin: { userInfo: userInfoFromStorage },
 }
 
