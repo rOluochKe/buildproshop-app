b9eb81fd6c0f9f3605b00de9230af9c42379b5af
Raymond Oluoch <rasholuoch@gmail.com>
2023-02-24 16:41:42 +0300

    api user authentication

--- backend/base/views/product_views.py
+++ backend/base/views/product_views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
-from .models import Product
-from .serializers import ProductSerializer
+from base.models import Product
+from base.serializers import ProductSerializer
 
-# Create your views here.
+from rest_framework import status
+
 
 @api_view(['GET'])
 def getProducts(request):
