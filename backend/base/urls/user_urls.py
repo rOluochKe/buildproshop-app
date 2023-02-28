4b7c7110251b0c8d331a15dc5c3f4ea1ea2e001b
Raymond Oluoch <rasholuoch@gmail.com>
2023-02-28 23:15:20 +0300

    admin screen part 1

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from base.views import user_views as views
-
+ 
 urlpatterns = [
   path('register/', views.registerUser, name="register"),
   path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
   path('', views.getUsers, name="users"),
   path('profile/', views.getUserProfile, name="users-profile"),
-  path('profile/update/', views.updateUserProfile, name="users-profile-update"),
+  path('profile/update/', views.updateUserProfile, name="users-profile-update"), 
+  path('<str:pk>/', views.getUserById, name='user'),
+  path('update/<str:pk>/', views.updateUser, name='user-update'),
+  path('delete/<str:pk>/', views.deleteUser, name='user-delete'),
 ]
--- client/src/store.js
+++ client/src/store.js
@@ -14,6 +14,9 @@ import {
   userRegisterReducer,
   userDetailsReducer,
   userUpdateProfileReducer,
+  userListReducer,
+  userDeleteReducer,
+  userUpdateReducer,
 } from './reducers/userReducers'
 
 import {
@@ -33,6 +36,9 @@ const reducer = combineReducers({
   userRegister: userRegisterReducer,
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
+  userList: userListReducer,
+  userDelete: userDeleteReducer,
+  userUpdate: userUpdateReducer,
 
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
